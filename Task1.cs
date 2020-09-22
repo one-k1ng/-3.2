f12c4d2f86a334dbcb538bde821456d5d546bf51
Администратор <@DESKTOP-PHORFLN>
2020-09-22 14:38:18 +0300

    Добавьте файлы проекта.

---
+++ Task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Практика3._2
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"boot.ini");
+            StreamWriter sw = new StreamWriter("boot - utf7.txt", false, Encoding.UTF7);
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+
+        }
+    }
+}
